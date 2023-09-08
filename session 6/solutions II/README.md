8fed2888b219e6b0cf1f28bec624e50907bdc7a4
Muthu Kamalan <muthukamalan98@gmail.com>
2023-09-08 10:42:10 +0530

    session 6 added and achieved results

---
+++ session 6/solutions II/README.md
@@ -0,0 +1,7 @@
+# WRITE IT AGAIN SUCH THAT IT ACHIEVES
+
+    1. 99.4% validation accuracy
+
+    2. Less than 20k Parameters
+    
+    3. Less than 20 Epochs
---
+++ session 6/solutions II/utils.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+
+from torchvision import datasets,transforms
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+
+device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+cuda   = torch.cuda.is_available()
+
+
+train_data = datasets.MNIST('../../data', train=True, download=False, transform=ToTensor())
+test_data = datasets.MNIST('../../data', train=False, download=False, transform=ToTensor())
+
+def show__random_img():
+    randnum = np.random.choice(train_data.targets.shape.numel())
+    # print(randnum)
+    fig = plt.figure()
+    plt.tight_layout()
+    plt.imshow(train_data.data[randnum].squeeze(0), cmap='gray')
+    plt.title(train_data.targets[randnum].item())
+    plt.show()
+
+
+
+def calculate_mean_std_mnist(datasets):
+    data_loader = DataLoader(datasets,batch_size=1,shuffle=False)
+    mean = torch.zeros(1);
+    std = torch.zeros(1)
+    num_samples = 0
+    transform = transforms.ToTensor()
+    for img in data_loader:
+        image = img[0]
+        image = image.squeeze()
+        mean += image.mean()             # mean across channel sum for all pics
+        std  += image.std()
+        num_samples += 1
+
+    mean /= num_samples
+    std /= num_samples
+    return (mean.item(),std.item())
