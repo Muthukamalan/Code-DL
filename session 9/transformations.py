deed77520fa4f16f9dab0d86e54f1cdb21a43dde
Muthu Kamalan <muthukamalan98@gmail.com>
2023-10-24 23:19:44 +0530

    session updated 9 failed and retrying again

--- session 9/transformations.py
+++ session 9/transformations.py
@@ -5,13 +5,15 @@ import numpy as np
 
 train_transforms = A.Compose([
     A.HorizontalFlip(always_apply=False,p=0.5),
-    A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=45, mask_value=(0.4914, 0.4822, 0.4465), always_apply=False,p=0.5,),
-    A.CoarseDropout( max_holes= 1,max_height = 16,max_width = 16,min_holes= 1,min_height = 16,min_width= 16,mask_fill_value = (0.4914, 0.4822, 0.4465),always_apply = False, p= 0.5),
-    A.Normalize(mean=(0.4914, 0.4822, 0.4465),std=(0.2470, 0.2435, 0.2616),p=1),
+    A.ShiftScaleRotate(),
+    A.Normalize(mean=(0.4914, 0.4822, 0.4465),std=(0.2470, 0.2435, 0.2616)),
+    A.CoarseDropout( max_holes= 1,max_height = 16,max_width = 16,min_holes= 1,min_height = 16,min_width= 16,fill_value = (0.4914, 0.4822, 0.4465)),
     ToTensorV2(),
 ])
 
 test_transforms = A.Compose([
-    A.Normalize(mean=(0.4914, 0.4822, 0.4465),std=(0.2470, 0.2435, 0.2616),p=1),
+    A.Normalize(mean=(0.4914, 0.4822, 0.4465),std=(0.2470, 0.2435, 0.2616)),
     ToTensorV2(),
 ])
+
+no_transforms = A.Compose([A.Normalize()])
--- session 9/utlis.py
+++ session 9/utlis.py
@@ -12,10 +12,8 @@ def denormalize(img):
     channel_means = (0.4914, 0.4822, 0.4465)
     channel_stdevs = (0.2470, 0.2435, 0.2616)
     img = img.astype(dtype=np.float32)
-
     for i in range(img.shape[0]):
         img[i] = (img[i] * channel_stdevs[i]) + channel_means[i]
-
     return np.transpose(img, (1, 2, 0))
 
 
