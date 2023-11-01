84de797191304167a1f7560150676df89f61ce94
Muthu Kamalan <muthukamalan98@gmail.com>
2023-11-02 01:08:31 +0530

    session 9 model with 83.25 accuracy

--- session 9/models.py
+++ session 9/models.py
@@ -4,11 +4,15 @@ from torch import functional as F
 import torch
 
 class DepthwiseConvLayer(nn.Module):
+    '''
+        Depthwise Conv + Pointwise Conv = Depthwise Seperable COnv 
+        depthwise have p=p and pointwiseConv has no padding to adjust total padding
+    '''
     def __init__(self,inc,outc,s,p,dp_rate)->None:
         super(DepthwiseConvLayer,self).__init__()
         self.layer = nn.Sequential(
-            nn.Conv2d(in_channels=inc,out_channels=inc,groups=inc,kernel_size=3,stride=s,padding=p,bias=False),
-            nn.Conv2d(in_channels=inc,out_channels=outc,kernel_size=1,padding=p,bias=False),
+            nn.Conv2d(in_channels=inc,out_channels=inc,groups=inc,kernel_size=3,stride=s,padding=p,bias=False, padding_mode='reflect'),
+            nn.Conv2d(in_channels=inc,out_channels=outc,kernel_size=1,padding=0,bias=False, padding_mode='reflect'),
             nn.ReLU(),
             nn.BatchNorm2d(outc),
             nn.Dropout2d(dp_rate)
@@ -31,7 +35,7 @@ class ConvLayer(nn.Module):
         super(ConvLayer,self).__init__()
 
         self.layer = nn.Sequential(
-            nn.Conv2d(in_channels=inc,out_channels=outc,kernel_size=3,padding=p,bias=False,dilation=d,stride=s),
+            nn.Conv2d(in_channels=inc,out_channels=outc,kernel_size=3,padding=p,bias=False,dilation=d,stride=s, padding_mode='reflect'),
             nn.BatchNorm2d(num_features=outc),
             nn.ReLU(inplace=True),
             nn.Dropout2d(dp_rate)
@@ -46,12 +50,13 @@ class TransBlock(nn.Module):
         self,
         inc:int,
         outc:int,
-        p:int
+        p:int,
+        s:int
     ):
         super(TransBlock,self).__init__()
 
         self.layer = nn.Sequential(
-            nn.Conv2d(in_channels=inc,out_channels=outc,kernel_size=1,padding=p,bias=False),
+            nn.Conv2d(in_channels=inc,out_channels=outc,kernel_size=1,stride=s,padding=p,bias=False, padding_mode='reflect'),
         )
     def forward(self,x):
         x = self.layer(x)
